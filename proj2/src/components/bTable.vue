2ad283af13493f64ec182cf97312ae9037d45c71
Mahesh-500apps <mahesh.atla@mantragroup.com>
2022-04-13 18:59:38 +0530

    Task(13-04-2022)

---
+++ proj2/src/components/bTable.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <b-table :items="items" :fields="fields"> </b-table>
+        </div>
+</template>
+<script>
+export default{
+    name:"MahI",
+  
+    data(){
+        return{
+            fields:['name','lastname'],
+            items:[
+                {name:"mahesh", lastname:"atla", age:23, job:"developer"},
+                {name:"sai", lastname:"varma", age:23, job:"test-engineer"},
+                {name:"krishna", lastname:"reddy", age:23, job:"data analyst"},
+                {name:"venky", lastname:"yadav", age:23, job:"manager"}
+            ]
+        }
+    }
+}
+</script>
