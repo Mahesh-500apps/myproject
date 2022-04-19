cbdad2239512b8a87b46d28b50627d0c627a81f6
Mahesh-500apps <mahesh.atla@mantragroup.com>
2022-04-19 19:22:14 +0530

    props

---
+++ proj2/src/components/api1.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="content"></div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data(){
+        return{
+
+        }
+    },
+  created() {
+    result=axios.get('http://universities.hipolabs.com/search?country')
+      console.log(result);
+    }
+  }
+
+</script>
---
+++ proj2/src/components/apis.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+   <h3>This is Rest Apis</h3>
+
+</div>
+</template>
+<script>
+const axios = require('axios').default;
+
+export default{
+    name:"ApiS",
+    data(){
+        return{
+
+        }
+    },
+    created(){
+     fetch("https://api.npms.io/v2/invalid-url")
+    .then(async response => {
+      const data = await response.json();
+
+      // check for error response
+      if (!response.ok) {
+        // get error message from body or default to response statusText
+        const error = (data && data.message) || response.statusText;
+        return Promise.reject(error);
+      }
+
+      this.totalVuePackages = data.total;
+    })
+    .catch(error => {
+      this.errorMessage = error;
+      console.error("There was an error!", error);
+    });
+
+    
+    }
+
+ 
+}
