12c6598a22b4d385f93d82ae0078c1ae135fb563
Mahesh-500apps <mahesh.atla@mantragroup.com>
2022-05-04 18:56:48 +0530

    4-may-22

---
+++ proj2/src/components/05-may-2022/crudop.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+<center>
+<b-row><b-col cols=5><Crudch :columns="['firstname','lastname','action']"
+:form-fields="{firstname:'',lastname:''}">
+
+     <template v-slot:input-fields="{ formdata }">
+<b-row>
+<b-col>
+first Name:<b-form-input id="input-2" v-model="formdata.firstname" required placeholder="Enter firstname"></b-form-input><br>
+second Name:<b-form-input id="input-2" v-model="formdata.lastname" required placeholder="Enter lastname" ></b-form-input><br>
+</b-col>
+</b-row>
+  </template>
+</Crudch>
+</b-col></b-row>
+</center>
+</div>
+</template>
+
+<script>
+import Crudch from './crudch.vue'
+export default {
+name:"MahI",
+components: { Crudch },
+};
+</script>
