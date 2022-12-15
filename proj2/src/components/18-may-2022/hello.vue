2add049eacc0c63ba7ec9f60d5b4f7de5bde05b7
mahesh.atla@mantragroup.com <sivamani.munaga@500apps.com>
2022-12-15 12:02:16 +0530

    raj

---
+++ proj2/src/components/18-may-2022/hello.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+        <h1>This is Heading Element</h1>
+        <p>This is Paragraph Element</p>
+        <button>{{message}}{{counter}}</button> 
+    </div>
+</template>
+
+<script>
+
+export default{
+    name:"HiP",
+    data(){
+    return {
+        message:'I am vue Developer',
+        counter:0,
+    }
+}
+}
+
+
+</script>
